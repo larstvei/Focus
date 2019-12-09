larstvei/Focus

commit d5631db0b6f78164e29bef6aa1b4950540c857d7

Remove `face`-suffix from faces

focus.el
@@ -59,12 +59,12 @@ Things that are defined include `symbol', `list', `sexp',
   :type '(float)
   :group 'focus)
 
-(defface focus-unfocused-face
+(defface focus-unfocused
   '((t :inherit font-lock-comment-face))
   "The face that overlays the unfocused area."
   :group 'focus)
 
-(defface focus-focused-face nil
+(defface focus-focused nil
   "The face that overlays the focused area."
   :group 'focus)
 

commit 2fbf024c53096add36f19bb8d267dc1c16494f9b

Fix bug introduced in d5631db

focus.el
@@ -131,8 +131,8 @@ It sets the `focus-pre-overlay', `focus-min-overlay', and
           focus-mid-overlay  (make-overlay (point-min) (point-max))
           focus-post-overlay (make-overlay (point-max) (point-max))
           focus-buffer (current-buffer))
-    (overlay-put focus-mid-overlay 'face 'focus-focused-face)
-    (mapc (lambda (o) (overlay-put o 'face 'focus-unfocused-face))
+    (overlay-put focus-mid-overlay 'face 'focus-focused)
+    (mapc (lambda (o) (overlay-put o 'face 'focus-unfocused))
           (list focus-pre-overlay focus-post-overlay))
     (add-hook 'post-command-hook 'focus-move-focus nil t)
     (add-hook 'change-major-mode-hook 'focus-terminate nil t)))